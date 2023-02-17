72cecc6bed095ba1ecc213e2c109f99742ecd63e
poojalawate1 <poojalawate1@gmail.com>
2023-02-17 09:42:32 +0530

    01_variableBasicsAssig.js
    01_variableBasicsAssig.js

---
+++ 01_variableBasicsAssig.js
@@ -0,0 +1,15 @@
+var num = 10;
+var Name ="Pooja Lawate";
+var isGradution = true;
+
+console.log('number is :' , num);
+console.log('typeof is :' ,typeof num);
+console.log('---------------------------');
+
+
+console.log('Name is :' , Name);
+console.log('typeof is :' , typeof Name);
+console.log('---------------------------');
+
+console.log('Gradution is :' ,  isGradution);
+console.log('typeof is :' ,  typeof isGradution);
