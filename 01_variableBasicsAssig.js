253e8e10930cd11b6e55cfccc1ccdc5b64f1a01b
poojalawate1 <poojalawate1@gmail.com>
2023-02-17 22:05:03 +0530

    01_variableBasicsAssign
    01_variableBasicsAssign

--- 01_variableBasicsAssig.js
+++ 01_variableBasicsAssig.js
@@ -6,22 +6,19 @@ console.log('number is :' , num);
 console.log('typeof is :' ,typeof num);
 console.log('---------------------------');
 
-
 console.log('Name is :' , Name);
 console.log('typeof is :' , typeof Name);
 console.log('---------------------------');
-
 console.log('Gradution is :' ,  isGradution);
 console.log('typeof is :' ,  typeof isGradution);
+
 console.log("---------------Null------------");
- 
 var age = null;
 console.log('Age is :' , age );
 console.log('typeof is :' ,  typeof  age);
 console.log("Null is an assigned value.");
+
 console.log("---------------Undefined------------");
-
-
 var lastName;
 console.log('lastName is :' ,lastName);
 console.log('typeof is :' ,  typeof  lastName);
