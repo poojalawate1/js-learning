fcebc68a10da403384b8dd7f0c86d7d4fdea5e00
poojalawate1 <poojalawate1@gmail.com>
2023-02-17 11:16:43 +0530

    02_Swap Variables
    02_Swap Variables

---
+++ 02_swapVariables.js
@@ -0,0 +1,42 @@
+console.log("*********************Swap To Name*************************");
+
+var name1 = "Sweety" ;
+var name2 = "Cutie";
+console.log("====================Before the swapping Names================");
+
+
+console.log( 'Name is : ' , name1);
+console.log('Name is : ', name2);
+console.log("====================After the swapping Names=================");
+
+
+var temp = name1 ;
+name1 = name2;
+name2 = temp;
+
+console.log( 'Name is : ' , name1);
+console.log('Name is : ', name2);
+
+
+console.log("*********************Swap To Number************************");
+console.log();
+var num1 = 100 ;
+var num2 = 200;
+var num3 = 300;
+console.log("===================Before the swapping number================");
+
+console.log("Number1 is :" , num1)
+console.log("Number2 is :" , num2);
+console.log("Number3 is :" , num3);
+
+console.log("====================After the swapping number===============");
+
+var temp = num1 ;
+num1 = num2;
+num2 = num3;
+num3=temp;
+
+console.log("Number1 is :" , num1)
+console.log("Number2 is :" , num2);
+console.log("Number3 is :" ,num3);
+
