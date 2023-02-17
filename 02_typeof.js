79ac65e9711e733bb54447fc43fe082f68c23efc
poojalawate1 <poojalawate1@gmail.com>
2023-02-17 21:59:30 +0530

    02_typeof Assign
    02_typeof Assign

---
+++ 02_typeof.js
@@ -0,0 +1,31 @@
+console.log('-------------Variable,Data,DataType---------------');
+console.log('------------Bank-------------');
+var bankName;
+bankName = "SBI Bank";
+console.log( ' Bank Name is :' , bankName);
+var typeis = typeof bankName;
+console.log( ' Data type is : ', typeis);
+console.log('-------------Mark--------------');
+
+var mark = '90%';
+console.log( ' Mark is : ', mark);
+console.log( ' Data type is :', typeof mark);
+console.log('-------------Working--------------');
+
+var isWorking = true;
+console.log( ' IsWorking  is :', isWorking );
+console.log( ' Data type is : ', typeof isWorking );
+console.log('-------------TotalCount--------------');
+
+var totalcount = 'Hundred and Seven';
+console.log(' TotalCount is : ',  totalcount );
+console.log( ' Data type is : ', typeof totalcount );
+
+console.log('------------Updated Count Number----------');
+totalcount = 107;
+console.log(' TotalCount is : ',  totalcount );
+console.log( ' Data type is : ', typeof totalcount );
+
+
+
+
