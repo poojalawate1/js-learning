d318591fc35519158eca768f5a53e5d93fdb948d
poojalawate1 <poojalawate1@gmail.com>
2023-02-17 21:45:33 +0530

    01_variable_assign

---
+++ 01_variable_assign.js
@@ -0,0 +1,42 @@
+console.log("============= Step 1 ============");
+
+var myFullName;
+myFullName = 'Pooja Chandrakant Lawate';
+console.log('My full name is:' , myFullName);
+
+myFullName = 'Pooja Lawate';
+console.log('My first and last name is:', myFullName);
+
+myFullName = 'Pooja Chandrakant Lawate';
+console.log( 'My first chandrakant Lawate is:', myFullName);
+
+console.log("============= Step 2 ============");
+
+var myLove = ' I love only JS';
+console.log( 'Result:',myLove);
+
+console.log("============= Step 3 ============");
+
+var firstName = "Pooja";
+console.log( 'First Name is:', firstName);
+
+var latName ="Lawate";
+console.log( 'Last Name is:', latName);
+
+var age = 24;
+console.log('Age  is:',age);
+
+var address = "Pandharpur";
+console.log("Address is:",address);
+
+var pin_address = 413317;
+console.log("Pin code is:" ,pin_address);
+
+var gender = 'Female';
+console.log('Gender is:', gender);
+
+var isMarried = 'No';
+console.log('Married status is:', isMarried );
+
+
+
