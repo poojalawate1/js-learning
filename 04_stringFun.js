485cf842784069aea823884ba1a4d268f8b47aa6
poojalawate1 <poojalawate1@gmail.com>
2023-02-21 14:39:52 +0530

    04_stringFun.js
    04_stringFun.js

---
+++ 04_stringFun.js
@@ -0,0 +1,22 @@
+function information(){
+    var company = "Tata Consultancy Services (TCS)";
+    console.log('My dream comapny is:', company);
+
+    var hobbies1 = "Reading Novels";
+    var hobbies2 = "Walking";
+    var hobbies3 = "Dancing "; 
+
+    console.log("My hobbies is:",hobbies1,`,`,hobbies2,'&',hobbies3);
+    var h1 = hobbies1.length;
+   console.log("Number of character 1st is:", h1);
+
+    var h2 = hobbies2.length;
+    console.log("Number of character 2nd is:", h2);
+    
+    var h3 = hobbies3.length;
+    console.log("Number of character 3rd is:", h3);
+
+ 
+}
+information();
+
