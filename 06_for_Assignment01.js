9709dd63405e6ce9e7735393c6c9421d86dbe944
poojalawate1 <poojalawate1@gmail.com>
2023-03-01 12:29:24 +0530

    For loop Assignment
    For loop Assignment

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,41 @@
+console.log(`1.WAP to print number from 5 to 15 by incrementing 1`);
+for( let i=5 ; i<=15 ; i++)
+{
+    console.log(`Print number from 5 to 15 by incrementing 1 : ${i}`);
+}
+
+console.log(`2.WAP to print numbers from 50 to 40 by decrementing by 1`);
+for( let i=50 ; i>=40 ; i--)
+{
+   console.log(`Print numbers from 50 to 40 by decrementing by 1 : ${i}`)
+}
+
+console.log(`3.WAP to find first 15 odd numbers`);
+for (let i=1 ; i<=15 ; i=i+2) 
+{
+  console.log(`First 15 odd numbers : ${i} `);   
+}   
+
+console.log(`4.WAP to find first 10 even numbers`);
+for(let i=0 ; i<=10 ; i=i+2)
+{
+        console.log(`First 10 even numbers : ${i}`)
+}
+
+console.log(`5.WAP to print table of 5`);
+for(let i=5 ; i<=50 ; i= i+5)
+{
+        console.log(`Print table of 5 : ${i}`);
+}
+
+console.log(`6.WAP to print table of 10 `);
+for(let i=10 ; i<=100 ; i= i+10)
+{
+          console.log(`Print table of 10 : ${i}`);
+}
+
+console.log(`7.WAP to print table of 10 in reverse order `);
+for(let i =100 ; i>=10 ; i--)
+{
+    console.log(`Print table of 10 in reverse : ${i}`);
+}
